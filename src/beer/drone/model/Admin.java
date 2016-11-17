1431962cc17cbfce9737b3104680714596f7e479
podmax <mj_melby@yahoo.com>
2016-11-16 23:09:34 -0700

    Update Admin.java

--- src/beer/drone/model/Admin.java
+++ src/beer/drone/model/Admin.java
@@ -1,13 +1,28 @@
 package beer.drone.model;
-
-/**
+/**Public subcalss of the User super class representing the admin in the system.
+ * 
  * @author Edward Seymour
+ * @version 1.0 (11/16/16)
  */
 public class Admin extends User {
+
+  /**
+   * Constructs a user.
+   *
+   * @param name the name of the admin
+   * @param email the email of the admin
+   * @param password the password of the admin
+   */
   public Admin(String name, String email, String password, Dispatch dispatch) {
     super(name, email, password, dispatch);
   }
 
+  /**
+   * Constructs a user.
+   *
+   * @param drone ...
+   * @param index ...
+   */
   public void addDrone(Drone drone, int index) {
     getDispatch().getWarehouse(index).addDrone(drone);
   }
