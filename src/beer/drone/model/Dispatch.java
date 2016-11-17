dc269ffb7ba9a91d63e309d9742758ef2ee04860
podmax <mj_melby@yahoo.com>
2016-11-16 23:51:51 -0700

    Update Dispatch.java

--- src/beer/drone/model/Dispatch.java
+++ src/beer/drone/model/Dispatch.java
@@ -3,8 +3,10 @@ package beer.drone.model;
 import java.util.ArrayList;
 import java.util.HashSet;
 
-/**
+/**Public calss representing the dispatch system.
+ * 
  * @author Edward Seymour
+ * @version 1.0 (11/16/16)
  */
 public class Dispatch {
   private HashSet<User> users;
@@ -14,7 +16,8 @@ public class Dispatch {
     users = new HashSet<>();
     warehouses = new ArrayList<>();
   }
-
+  /** @param order customers order. 
+  */
   public void makeOrder(Order order) {
     /* TODO: Select closest warehouse */
     for(Warehouse warehouse : warehouses) {
@@ -22,15 +25,19 @@ public class Dispatch {
       break;
     }
   }
-
+  /** @param warehouse warehouse. 
+  */
   public void addWarehouse(Warehouse warehouse) {
     warehouses.add(warehouse);
   }
-
+  /** @param user system user. 
+  */
   public void addUser(User user) {
     users.add(user);
   }
-
+  /** @param index customers order. 
+  * @return warehouse number
+  */
   public Warehouse getWarehouse(int index) {
     return warehouses.get(index);
   }
