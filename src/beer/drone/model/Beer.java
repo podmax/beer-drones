a5f2627590cce443df38aa08916b68494a10ac7d
podmax <mj_melby@yahoo.com>
2016-11-16 23:15:16 -0700

    Update Beer.java

--- src/beer/drone/model/Beer.java
+++ src/beer/drone/model/Beer.java
@@ -1,7 +1,9 @@
 package beer.drone.model;
 
-/**
+/**Public calss representing the variations of beer.
+ * 
  * @author Edward Seymour
+ * @version 1.0 (11/16/16)
  */
 public class Beer {
   private int cost;
@@ -9,7 +11,15 @@ public class Beer {
   private int kind;
   private int packSize;
   private int quantity;
-
+  /**
+   * Constructs a beer.
+   *
+   * @param cost the cost of the beer.
+   * @param description description of beer.
+   * @param kind type of beer.
+   * @param packSize number of beers in a pack.
+   & @param quantity number of beer available.
+   */
   public Beer(int cost, String description, int kind, int packSize, int quantity) {
     this.cost = cost;
     this.description = description;
