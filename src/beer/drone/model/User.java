eaf1ba326bba7f47e3e1b4cb1bf778d8e0f2fa22
podmax <mj_melby@yahoo.com>
2016-11-17 00:28:58 -0700

    Update User.java

--- src/beer/drone/model/User.java
+++ src/beer/drone/model/User.java
@@ -4,6 +4,7 @@ package beer.drone.model;
  * User is an abstract class that represents a user in the system
  *
  * @author Edward Seymour
+ * @version 1.0 (11/16/16)
  */
 public abstract class User {
   private String name;
@@ -28,19 +29,23 @@ public abstract class User {
   public void register() {
     dispatch.addUser(this);
   }
-
+  /** @return returns name of user.
+  */
   public String getName() {
     return name;
   }
-
+  /** @return returns email of user.
+  */
   public String getEmail() {
     return email;
   }
-
+  /** @return returns password of user.
+  */
   public String getPassword() {
     return password;
   }
-
+  /** @return returns dispatch.
+  */
   public Dispatch getDispatch() {
     return dispatch;
   }
