5e64e1243050ff4fa394917caf5adde1ebe5d257
podmax <mj_melby@yahoo.com>
2016-11-17 00:26:45 -0700

    Update Order.java

--- src/beer/drone/model/Order.java
+++ src/beer/drone/model/Order.java
@@ -2,14 +2,18 @@ package beer.drone.model;
 
 import java.util.Date;
 
-/**
+/**Public class representing the customers order.
+ *
  * @author Edward Seymour
+ * @version 1.0 (11/16/16)
  */
 public class Order {
   private Date timePlaced;
   private boolean delivered;
   private Beer beer;
-
+  /**
+   * @param beer @see Beer class.
+   */
   public Order(Beer beer){
     this.beer = beer;
     delivered = false;
